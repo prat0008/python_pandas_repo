60c50a38b51bd8aaf34c52a156bdcd5067c78eb7
prat0008 <prateekgoel142@gmail.com>
2025-09-07 22:22:23 +0530

    updated

--- pandas_pd.py
+++ pandas_pd.py
@@ -10,4 +10,18 @@ urlretrieve(url, 'italy-covid-daywise.csv')
 # Now read the saved file
 covid_df = pd.read_csv('italy-covid-daywise.csv')
 
-print(covid_df.head())
+#prints the data from the csv file
+#print(covid_df)
+
+#to view some basic information about the dataframe we use .info method
+#print(covid_df.info())
+
+#to view the statistical info like mean,standard deviation,min/max values,
+#and no. of non empty values using the .describe method.
+#print(covid_df.describe())
+
+#to retrieve the no. of rows and columns within the df.
+#print(covid_df.shape)
+
+#columns property contains the list of columns within the df.
+print(covid_df.columns)
